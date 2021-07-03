d7366deab1942d26997067a6cc977b59673ed2a4
Justin Goats <justingoats@yahoo.com>
2021-07-02 20:17:06 -0700

    created all required middleware

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
+let passport = require("passport");
+let passportLocal = require("passport-local").Strategy;
+let cookieParser = require("cookie-parser");
+let bcrypt = require("bcryptjs");
+let session = require("express-session");
 let cors = require("cors");
 let Models = require("./mongoose/user.js");
 let Users = Models.User;
@@ -9,7 +14,17 @@ let PORT = process.env.PORT || 250;
 let CONNECT = process.env.CONNECTION_STRING;
 
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser(process.env.SECRET));
 
 mongoose.connect(CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('open', function (ref) {
@@ -39,7 +54,13 @@ app.get("/users/:name", (req, res) => {
             res.send(err);
             console.log(err);
         })
-})
+});
+app.post("/login", (req, res) => {
+    console.log(req.body);
+});
+app.post("/register", (req, res) => {
+    console.log(req.body);
+});
 app.listen(PORT, (err) => {
     if (err) {
         console.log(err)
